be3715088c34f1742681f5e6c3d009e94b513c64
Numero Uno <numerounodeveloper@gmail.com>
2023-05-22 18:40:26 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const SearchForm = ({ onSearch }) => {
+  const [criteria, setCriteria] = useState('');
+  const [criteriaValue, setCriteriaValue] = useState('');
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    onSearch(criteria, criteriaValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <form onSubmit={handleSearch}>
+      <div>
+        <label>Search By:</label>
+        <select
+          value={criteria}
+          onChange={(e) => setCriteria(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="">Select Criteria</option>
+          <option value="restaurantname">Restaurant</option>
+          <option value="menuitem">Menu Item</option>
+        </select>
+      </div>
+      <div>
+        <label>Search Value:</label>
+        <input
+          type="text"
+          value={criteriaValue}
+          onChange={(e) => setCriteriaValue(e.target.value)}
+        />
+      </div>
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+const SearchResult = ({ results }) => {
+  //console.log(results);
+  
+  if (results.length === 0) {
+    return <p>No results found.</p>;
+  }
+
+  return (
+    <div>
+      <h2>Search Results:</h2>
+      <ul>
+        {results.map((result) => (
+          <li key={result.restaurantName + result.itemName }>
+            <strong>Restaurant:</strong> {result.restaurantName}
+            <br />
+            <strong>Item Name:</strong> {result.itemName}
+            <br />
+            <strong>Price:</strong> {result.price}
+            <br />
+            <strong>Rating:</strong> {result.Ratings}
+            <br />
+            <strong>Address:</strong> {result.address}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = (criteria, criteriaValue) => {
+    if (criteria && criteriaValue) {
+      fetch(`http://localhost:9004/food/api/v1/user/${criteria}/${criteriaValue}`)
+        .then((response) =>  response.json())
+        .then((data) => setSearchResults(data))
+        .catch((error) => console.error(error));
+    }
+  };
+
+  return (
+    <div>
+      <h1>Food Search</h1>
+      <SearchForm onSearch={handleSearch} />
+      <SearchResult results={searchResults} />
+    </div>
+  );
+};
 
 export default App;
